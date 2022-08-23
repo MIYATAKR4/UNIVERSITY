17dc35ff51628b1fc93e02cd13556a3a816f2e7c
Catarina Martins Diniz <62525104+MIYATAKR4@users.noreply.github.com>
2022-08-23 17:14:55 -0300

    Update and rename ALBERGUE DO KPT.py to Ficha de registro pra... algo ai.py

--- Ficha de registro pra... algo ai.py
+++ Ficha de registro pra... algo ai.py
@@ -4,6 +4,6 @@ Age = int(input('Idade:'))
 Year_death = int(input('Ano que morreu:'))
 Ser_Number = Age + Year_death * 666
 
-print('Registrado. seu número é:', Ser_Number,'!!', 'Mostre isso para algum funcionário e eles te levarão à sua cama.')
+print('Registrado. seu número é:', Ser_Number,'!!', 'Mostre isso para algum funcionário e eles te direcionarão à sua cama!')
 
 
