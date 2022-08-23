0b8570bebab32da23cb189766d001ba07f369bee
Catarina Martins Diniz <62525104+MIYATAKR4@users.noreply.github.com>
2022-08-23 17:14:09 -0300

    Add files via upload

---
+++ ALBERGUE DO KPT.py
@@ -0,0 +1,9 @@
+print('XXXXX ALBERGUE DO CAPETA.INC XXXXXX')
+Name = input('Nome:')
+Age = int(input('Idade:'))
+Year_death = int(input('Ano que morreu:'))
+Ser_Number = Age + Year_death * 666
+
+print('Registrado. seu número é:', Ser_Number,'!!', 'Mostre isso para algum funcionário e eles te levarão à sua cama.')
+
+
