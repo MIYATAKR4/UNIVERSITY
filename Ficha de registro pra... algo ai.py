034ee8db6e478fcc1ea890ad309f93bfe5e0722d
Catarina Martins Diniz <62525104+MIYATAKR4@users.noreply.github.com>
2022-08-24 16:16:23 -0300

    Delete Ficha de registro pra... algo ai.py

--- Ficha de registro pra... algo ai.py
+++
@@ -1,9 +0,0 @@
-print('XXXXX ALBERGUE DO CAPETA.INC XXXXXX')
-Name = input('Nome:')
-Age = int(input('Idade:'))
-Year_death = int(input('Ano que morreu:'))
-Ser_Number = Age + Year_death * 666
-
-print('Registrado. seu número é:', Ser_Number,'!!', 'Mostre isso para algum funcionário e eles te direcionarão à sua cama!')
-
-
