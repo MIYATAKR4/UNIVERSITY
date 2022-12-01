c9d8b0bd462f0678986f0e0bf9b330747dceb1a1
Catarina Martins Diniz <62525104+MIYATAKR4@users.noreply.github.com>
2022-12-01 19:25:37 -0300

    Create tickets.py
    Uma atividade usando essa tabela aqui pra treinar conceitos matemáticos no python
    
    Dia	Valor Total Vendas	Qtd Total Vendas	Ticket Medio
    19/01	(A)	                      3	                  320.52
    20/01	834.47	             (B)	                  119.21
    23/01	15378.12	              5	                     (C)

---
+++ tickets.py
@@ -0,0 +1,22 @@
+# (A)
+qtd_total_vendas19 = 3
+ticket_medio19 = 320.52
+valor_total_vendas19 = ticket_medio19 * qtd_total_vendas19
+
+print(valor_total_vendas19)
+
+# (B)
+valor_total_vendas20 = 834.47
+ticket_medio20 = 119.21
+qtd_total_vendas20 = valor_total_vendas20 / ticket_medio20
+
+
+print(int(qtd_total_vendas20))
+
+# (C)
+valor_total_vendas21 = 15378.12
+qtd_total_vendas21 = 5
+ticket_medio21 = valor_total_vendas21 / qtd_total_vendas21
+
+print(ticket_medio21)
+
